d0e9cab0cc97122eccef9a385b1d09be5f87b820
BAEK <100jaehun@nave.com>
2019-06-09 12:20:40 +0900

    Router 및 기본요소 추가
    IndexPage
    SigninPage
    SignupPage
    Header
    Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-
-
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MainPage from'./page/MainPage';
+import SigninPage from'./page/SigninPage';
+import SignupPage from'./page/SignupPage';
+import Header from './component/header/Header';
+import Footer from './component/footer/Footer';
 class App extends Component {
   render() {
     return (
-      <div>
-        app
-    </div>
+      <Router>
+        <div>
+          <Header/>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/singin/" component={SigninPage} />
+          <Route path="/singup/" component={SignupPage} />
+          <Footer/>
+        </div>
+      </Router>
     )
   }
 }
